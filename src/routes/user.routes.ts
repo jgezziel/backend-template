aaf8f3ded380c5e75108e40dd751a8c41a97a952
jgezziel <gzl1910@gmail.com>
2024-08-16 13:12:53 -0600

    add example use auth controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import userControllers from "@controllers/user.controllers";
+import { verifyToken } from "@middlewares/auth";
 
 const userRoutes: Router = Router();
 
 userRoutes.get("/", userControllers.getUsers);
-userRoutes.get("/:id", userControllers.getUserById);
+userRoutes.get("/:id", verifyToken, userControllers.getUserById);
 userRoutes.post("/", userControllers.createUser);
 // userRoutes.put("/:id", userControllers.updateUser);
 // userRoutes.delete("/:id", userControllers.deleteUser);
